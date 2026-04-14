a85387649ecf925e882c481ffb22cddf76ee5380
Xolani Richards <xolani83@gmail.com>
2026-04-14 18:07:25 +0900

    fixed

--- Assets/Scripts/Abilities/Targeting/ProjectileTargeting.cs
+++ Assets/Scripts/Abilities/Targeting/ProjectileTargeting.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "ProjectileTargeting", menuName = "ROGUE/TargetingStrategy/Projectile", order = 0)]
 public class ProjectileTargeting : TargetingStrategy {
     public GameObject projectilePrefab;
     public float projectileSpeed = 10f;
 
-    public override void OnStart(Ability ability, TargetingManager targetingManager) {
+    public override void OnStart(Ability ability, TargetingManager targetingManager) 
+    {
         this.ability = ability;
         this.targetingManager = targetingManager;
 
         if (projectilePrefab != null) {
             Vector3 pos = targetingManager.transform.position + new Vector3(0,1,0); 
-            var flatForward = targetingManager.cam.transform.forward;
+            // var flatForward = targetingManager.cam.transform.forward;
+            var flatForward = targetingManager.transform.forward;
             flatForward.y = 0;
             flatForward.Normalize();
             
