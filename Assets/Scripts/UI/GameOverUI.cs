ce765b3e39431dbd87f3ee89eb6d4ae3fbf682dd
Xolani Richards <xolani-richards@matso.com>
2026-04-23 16:44:36 +0900

    fixed ui order

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -5,11 +5,12 @@ public class GameOverUI : MonoBehaviour
     public void OnRetry ()
     {
         GameManager.Instance.ReloadLevel();
+        Destroy(gameObject, 0.2f);
     }
 
     public void OnReturn ()
     {
-        Debug.Log("ALSO CLICKED");
         GameManager.Instance.ReturnToMainMenu();
+        Destroy(gameObject, 0.2f);
     }
 }
